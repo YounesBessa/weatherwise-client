8eee8113663e580d65d3665a9562d0a7d8f95a50
Younes Bessa <bessa460@gmail.com>
2023-07-12 03:05:19 +0200

    🔧 update next config

--- next.config.js
+++ next.config.js
@@ -3,5 +3,10 @@ const withPWA = require("next-pwa");
 module.exports = withPWA({
   pwa: {
     dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+    scope: "/app",
+    sw: "service-worker.js",
   },
 });
